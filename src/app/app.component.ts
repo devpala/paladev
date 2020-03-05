b16682d17ec7d6238f960c7650cdbfc87eeb84df
Rubén Palavecino <rubenpalavecino@localhost.localdomain>
2020-03-05 03:19:56 -0300

    auto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend-paladesing';
+  message:string = '';
+  @ViewChild('codeValue', { static: false }) code_value: ElementRef;
+  numeros:number[] = [1,2,3,4,5,6,8,9,10]
+  
+  numero_de_columna:number = 1;
+  posicion:number = 0;
+
+  restar(){
+    if(this.posicion === 1) {
+      this.numero_de_columna = 1;
+    } else {
+      this.numero_de_columna = this.numeros[this.posicion--];
+    }
+  }
+  
+  sumar():void {
+    if(this.posicion === 9){
+      this.numero_de_columna = 10;
+    } else {
+      this.numero_de_columna = this.numeros[this.posicion++];
+    } 
+  }
+
+  copy() {
+    const boton:HTMLInputElement =  this.code_value.nativeElement as HTMLInputElement;
+    const pre = boton.querySelectorAll('pre');
+    console.log(pre);
+    document.execCommand('copy');
+    this.message = 'copiado!';
+  }
 }
